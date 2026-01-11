46483efad399a011c5ef504cec23b22ba7f46ddb
habibidipopadi <97330751+habibidipopadi@users.noreply.github.com>
2026-01-10 21:35:46 -0500

    Fix import paths in crawl_all.py to use correct relative imports from parent package

--- src/agent/crawl_all.py
+++ src/agent/crawl_all.py
@@ -1,8 +1,8 @@
 import asyncio
 
-from .sources.github import search_github_tools
-from .sources.pypi import search_pypi_tools
-from .ranking import rank_tools
+from ..sources.github import search_github_tools
+from ..sources.pypi import search_pypi_tools
+from ..ranking import rank_tools
 
 
 async def crawl_all():
